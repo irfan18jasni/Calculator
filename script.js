b97005fe3b2868a25288374c56ef0471a3d64f16
IrfanJasni <148796478+irfan18jasni@users.noreply.github.com>
2024-11-26 20:51:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function add(){
+    // Get 2 use input numbers
+    // put parseFloat at the top is better, so you can change it.
+    const Num1 = parseFloat(prompt("Enter your first number"))
+    const Num2 = parseFloat(prompt("Enter your second number"))
+
+    // Perform calculations
+    const result = Num1 + Num2
+
+    // Perform result
+    alert(`${Num1} + ${Num2} = ${result}`)
+}
+
+function minus(){
+    // Get 2 use input numbers
+    // put parseFloat at the top is better, so you can change it.
+    const Num1 = parseFloat(prompt("Enter your first number"))
+    const Num2 = parseFloat(prompt("Enter your second number"))
+
+    // Perform calculations
+    const result = Num1 + Num2
+
+    // Perform result
+    alert(`${Num1} + ${Num2} = ${result}`)
+}
+
+function multiply(){
+    // Get 2 use input numbers
+    // put parseFloat at the top is better, so you can change it.
+    const Num1 = parseFloat(prompt("Enter your first number"))
+    const Num2 = parseFloat(prompt("Enter your second number"))
+
+    // Perform calculations
+    const result = Num1 + Num2
+
+    // Perform result
+    alert(`${Num1} + ${Num2} = ${result}`)
+}
+
+function divide(){
+    // Get 2 use input numbers
+    // put parseFloat at the top is better, so you can change it.
+    const Num1 = parseFloat(prompt("Enter your first number"))
+    const Num2 = parseFloat(prompt("Enter your second number"))
+
+    // Perform calculations
+    const result = Num1 + Num2
+
+    // Perform result
+    alert(`${Num1} + ${Num2} = ${result}`)
+}
